6f047ce37f28b46e997101ac23fd5d43bde659e0
Mark <mark@opifex.ca>
2022-01-26 10:22:06 -0500

    Fix to clocking code to get stepped down gclks to work

--- hal/src/thumbv7em/clock/v2/gclk.rs
+++ hal/src/thumbv7em/clock/v2/gclk.rs
@@ -150,6 +150,7 @@ impl<G: GclkNum> GclkToken<G> {
     #[inline]
     fn improve_duty_cycle(&mut self, flag: bool) {
         self.genctrl().modify(|_, w| w.idc().bit(flag));
+        self.wait_syncbusy();
     }
 
     /// Enable output of the generator clock over [`GCLK_IO`][GclkIo] pins
