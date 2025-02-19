4281f69b1f419c07f1bcb431e199c8452dc1719e
Mark <mark@opifex.ca>
2025-02-19 16:32:53 -0500

    Add PartialEq derive to SPI error type

--- hal/src/sercom/spi.rs
+++ hal/src/sercom/spi.rs
@@ -441,7 +441,7 @@ impl TryFrom<Status> for () {
 ///
 /// The SPI peripheral only has two error types, buffer overflow and transaction
 /// length error.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Error {
     Overflow,
     LengthError,
